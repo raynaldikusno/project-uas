458e2105b4d8d491afa4faf2f5ff0400eea8d9d2
raynaldikusno <aldia3344@gmail.com>
2024-06-04 14:04:13 +0700

    new changes internet banking

--- internet-banking/routes/web.php
+++ internet-banking/routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('//home');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function () {
+});
