3de55fbb8c956b6e22e15f8c6b29d876009519f2
raynaldikusno <aldia3344@gmail.com>
2024-06-07 10:05:30 +0700

    new updated internet-banking

---
+++ internet-banking/app/Http/Controllers/AccountController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+    public function index()
+    {
+        $accountDetails = [
+            'account_number' => '1234567890',
+            'balance' => 5000.00,
+        ];
+
+        return view('accounts.index', compact('accountDetails'));
+    }
+}
+
--- internet-banking/routes/web.php
+++ internet-banking/routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('//home');
-});
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\TransferController;
+use App\Http\Controllers\HomeController;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::middleware('auth')->group(function () {
-});
+// Define the routes for the different pages
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/accounts', [AccountController::class, 'index'])->name('accounts')->middleware('auth');
+Route::get('/transfer', [TransferController::class, 'create'])->name('transfer')->middleware('auth');
+Route::post('/transfer', [TransferController::class, 'store'])->middleware('auth');
