a5033d20c3277b6bab616b8341f87d4a51bfffde
raynaldikusno <aldia3344@gmail.com>
2024-06-15 00:25:10 +0700

    updated

--- internet-banking/resources/views/welcome.blade.php
+++ internet-banking/resources/views/welcome.blade.php
@@ -28,7 +28,10 @@
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Internet Banking</a>
+    <a class="navbar-brand" href="#">
+        <img src="images/gambar.jpg" style="height: 30px; width: auto;">
+        TurtleBank
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
